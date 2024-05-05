c69d3873b76b3422cfe8209c61d9c8e7e30fb3d8
maximecq <maximecq@gmail.com>
2024-05-06 00:57:04 +0200

    sauvegarde index style script

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// document.getElementById("accueil").addEventListener("click", function() {
-//     // Redirection vers une nouvelle page HTML
-//     window.location.href = "carte.html";
-// });
 
 var btnlogo = document.getElementById("accueil");
 var btn1 = document.getElementById("accueil");
 var btn2 = document.getElementById("carte");
 var btn3 = document.getElementById("conseille");
+var btn4 = document.getElementById("photo");
+var btn5 = document.getElementById("FAQ");
+var btn6 = document.getElementById("inscription");
+var btn7 = document.getElementById("connexion");
+
 
 btnlogo.addEventListener("click", function() {
     window.location.href = "index.html";
@@ -23,3 +24,20 @@ btn2.addEventListener("click", function() {
 btn3.addEventListener("click", function() {
     window.location.href = "conseille.html";
 });
+
+btn4.addEventListener("click", function() {
+    window.location.href = "photo.html";
+});
+
+btn5.addEventListener("click", function() {
+    window.location.href = "FAQ.html";
+});
+
+btn6.addEventListener("click", function() {
+    window.location.href = "inscription.html";
+});
+
+btn7.addEventListener("click", function() {
+    window.location.href = "connexion.html";
+});
+
