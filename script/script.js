28b82edc00dbf6607dc25442f2186aea586326fd
maximecq <maximecq@gmail.com>
2024-06-21 12:47:11 +0200

    include du header et footer dans index.html

---
+++ script/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch('./page/autre/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('header').innerHTML = data;
+        })
+        .catch(error => console.error('Erreur lors du chargement du header:', error));
+
+
+    fetch('./page/autre/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Erreur lors du chargement du footer:', error));
+});
+
