a65d0cb4075cbd5e34437f9b5ffa964a70f8e7ee
maximecq <maximecq@gmail.com>
2024-06-23 20:38:15 +0200

    modification pour la requete http header and footer

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     fetch('./page/autre/header.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
+            loadCSS('./css/header.css');
         })
         .catch(error => console.error('Erreur lors du chargement du header:', error));
-
 
     fetch('./page/autre/footer.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('footer').innerHTML = data;
+            loadCSS('./css/footer.css');
         })
         .catch(error => console.error('Erreur lors du chargement du footer:', error));
 });
