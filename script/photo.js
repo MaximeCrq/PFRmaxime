cced5481a0e69fd52e5fdf2a56c26b52033e8aab
maximecq <maximecq@gmail.com>
2024-09-22 21:00:59 +0200

    modif photo

--- script/photo.js
+++ script/photo.js
@@ -1,70 +1,28 @@
-// const photo1 = document.getElementById('image1');
-// const photo2 = document.getElementById('image2');
-// const photo3 = document.getElementById('image3');
-// const photo4 = document.getElementById('image4');
-// const photo5 = document.getElementById('image5');
-
-// //NOMBRE de photo, à modifier en fonction !!!
-// const nbrPhoto = 5;
+const containerP = document.getElementById('section-photo');
+const tabP = containerP.querySelectorAll('img');
+const tabPArray = Array.from(tabP);
 
 
-// //fonction mettre les photos dans un tableau
-// let tabPhoto = [];
 
-// for (i=1 ; i<=nbrPhoto ; i++) {
-//     const photoI = document.querySelector(`#photo${i}`);
-//     tabPhoto.push(photoI)
-// }
+tabPArray.forEach(img => {
+    img.addEventListener('mouseover', () => {
+        //autre img
+        const otherImages = tabPArray.filter(image => image !== img);
+        // Definition filter() :
+        //filter() est une méthode des tableaux en JavaScript.
+        // Elle prend une fonction de rappel (callback function)
+        // comme argument et crée un nouveau tableau avec tous
+        // les éléments qui passent un test
 
-// //fonction pour voir le nom des facteur de l'élements (taille, autre)
-// const aze = document.getElementsByClassName('section-photo');
-// console.log(aze);
+        otherImages.forEach(otherImg => {
+            otherImg.style.opacity = '0.5'; 
+        });
+    });
 
-// //fonction déterminer le type de photo (1, 2, ou 3)
-// for (i=1 ; i<=nbrPhoto ; i++) {
-//     //initialisation du type
-//     let type = 0;
-
-//     const photoI = document.querySelector(`#photo${i}`);
-
-//     //vérification du type 1
-//     if ((photoI.clientHeight + (photoI.clientWidth/10) ) < photoI.clientWidth) {
-//         type = 1;
-//     }
-
-//     //vérification du type 2
-//     if (photoI.clientHeight > (photoI.clientWidth + (photoI.clientHeight/10) )) {
-//         type = 2;
-//     }
-
-//     //vérification du type 3
-//     //condition de 10%
-//     let c1 = 0;
-//     let c2 = 0;
-
-//     if (photoI.clientHeight-photoI.clientWidth < 0) {
-//         c1 = -(photoI.clientHeight-photoI.clientWidth);
-//     } else {
-//         c1 = photoI.clientHeight-photoI.clientWidth;
-//     }
-//     if (photoI.clientHeight > photoI.clientWidth) {
-//         c2 = photoI.clientHeight;
-//     }   else {
-//         c2 = photoI.clientWidth;
-//     }
-//     if (
-//         photoI.clientHeight == photoI.clientWidth ||
-//         c1 < (c2/10)
-//     ) 
-//         {
-//         type = 3;
-//     }
-//     //Affichage taille/type photo
-//     console.log(photoI.clientHeight, photoI.clientWidth, type);
-
-//     let posX = 0;
-//     let posY = 0
-//     if (type = 1) {
-        
-//     }
-// }
+    img.addEventListener('mouseout', () => {
+        //restauration parametre autre img
+        tabPArray.forEach(otherImg => {
+            otherImg.style.opacity = '1';
+        });
+    });
+});
